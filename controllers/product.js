b115bf29b807b8289f9db64c48cb10f7b754033a
amine <zouariamine52@gmail.com>
2023-09-02 14:54:29 +0100

    product get put post

--- controllers/product.js
+++ controllers/product.js
@@ -35,3 +35,48 @@ exports.remove = async (req, res) => {
     return res.staus(400).send('Product delete failed')
   }
 }
+
+exports.read = async (req, res) => {
+  const product = await Product.findOne({ slug: req.params.slug })
+    .populate('category')
+    .populate('subs')
+    .exec()
+  res.json(product)
+}
+
+exports.update = async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title)
+    }
+    const updated = await Product.findOneAndUpdate(
+      { slug: req.params.slug },
+      req.body,
+      { new: true }
+    ).exec()
+    res.json(updated)
+  } catch (err) {
+    console.log('PRODUCT UPDATE ERROR ----> ', err)
+    // return res.status(400).send("Product update failed");
+    res.status(400).json({
+      err: err.message,
+    })
+  }
+}
+
+exports.list = async (req, res) => {
+  try {
+    // createdAt/updatedAt, desc/asc, 3
+    const { sort, order, limit } = req.body
+    const products = await Product.find({})
+      .populate('category')
+      .populate('subs')
+      .sort([[sort, order]])
+      .limit(limit)
+      .exec()
+
+    res.json(products)
+  } catch (err) {
+    console.log(err)
+  }
+}
