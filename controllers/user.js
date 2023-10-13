7f0045481299055e9469be707d4556174c9e3df6
amine <zouariamine52@gmail.com>
2023-10-13 01:00:23 +0100

    checkout

---
+++ controllers/user.js
@@ -0,0 +1,77 @@
+const User = require("../models/user");
+const Product = require("../models/product");
+const Cart = require("../models/cart");
+
+exports.userCart = async (req, res) => {
+  // console.log(req.body); // {cart: []}
+  const { cart } = req.body;
+
+  let products = [];
+
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  // Check if a cart with the logged-in user ID already exists and remove it if found
+  await Cart.findOneAndDelete({ orderdBy: user._id }).exec();
+
+  for (let i = 0; i < cart.length; i++) {
+    let object = {};
+
+    object.product = cart[i]._id;
+    object.count = cart[i].count;
+    object.color = cart[i].color;
+    // get price for creating total
+    let { price } = await Product.findById(cart[i]._id).select("price").exec();
+    object.price = price;
+
+    products.push(object);
+  }
+
+  // console.log('products', products)
+
+  let cartTotal = 0;
+  for (let i = 0; i < products.length; i++) {
+    cartTotal = cartTotal + products[i].price * products[i].count;
+  }
+
+  // console.log("cartTotal", cartTotal);
+
+  let newCart = await new Cart({
+    products,
+    cartTotal,
+    orderdBy: user._id,
+  }).save();
+
+  console.log("new cart", newCart);
+  res.json({ ok: true });
+};
+
+exports.getUserCart = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  let cart = await Cart.findOne({ orderdBy: user._id })
+    .populate("products.product", "_id title price totalAfterDiscount")
+    .exec();
+
+  res.json({
+    products: cart?.products || [],
+    cartTotal: cart?.cartTotal || 0,
+    totalAfterDiscount: cart?.totalAfterDiscount || 0,
+  });
+};
+
+exports.emptyCart = async (req, res) => {
+  console.log("empty cart");
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  const cart = await Cart.findOneAndRemove({ orderdBy: user._id }).exec();
+  res.json(cart);
+};
+
+exports.saveAddress = async (req, res) => {
+  const userAddress = await User.findOneAndUpdate(
+    { email: req.user.email },
+    { address: req.body.address }
+  ).exec();
+
+  res.json({ ok: true });
+};
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,20 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/user', (req, res) => {
-  res.json({
-    data: 'hey you hit user API endpoint',
-  })
-})
+// middlewares
+const { authCheck } = require("../middlewares/auth");
+// controllers
+const {
+  userCart,
+  getUserCart,
+  emptyCart,
+  saveAddress,
+} = require("../controllers/user");
 
-module.exports = router
+router.post("/user/cart", authCheck, userCart); // save cart
+router.get("/user/cart", authCheck, getUserCart); // get cart
+router.delete("/user/cart", authCheck, emptyCart); // empty cart
+router.post("/user/address", authCheck, saveAddress);
+
+module.exports = router;
